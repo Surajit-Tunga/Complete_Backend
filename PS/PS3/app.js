ef5f4d57562a746f6f58ebb5448d1bddac85ef0d
Surajit Tunga <surajittunga2005@gmail.com>
2025-07-14 22:41:23 +0530

    Solved PS3

--- PS/PS3/app.js
+++ PS/PS3/app.js
@@ -2,6 +2,48 @@ const express = require('express');
 
 const app = express();
 
+// middleware 1 
+app.use((req, res, next)=> {
+    console.log("First Middleware",req.url, req.method);
+    next();
+})
+
+// middleware 2
+app.use((req, res, next)=> {
+    console.log("Second Middleware",req.url, req.method);
+    next();
+})
+
+// middleware 3
+// app.use((req, res, next)=> {
+//     console.log("Third Middleware",req.url, req.method);
+//     res.send("<p>Welcome to 3rd Middleware</p>")
+// })
+
+app.get("/",(req, res, next)=> {
+    console.log("/ Middleware",req.url, req.method);
+   res.send("<p>Welcome to / Middleware</p>")
+})
+
+app.get("/contact",(req, res, next)=> {
+    console.log("contact Middleware",req.url, req.method);
+   res.send(`
+    <p>contact us</p>
+    <form action="/contact" method="POST">
+      <input type="text" name="name" placeholder="Enter your name" />
+      <input type="email" name="email" placeholder="Enter your email" />
+      <input type="submit"/>
+    </form>
+    
+    
+    `)
+})
+
+app.post("/contact",(req, res, next)=> {
+    console.log("contact post Middleware",req.url, req.method);
+     res.send("<p>Thank you for contact us.</p>")
+})
+
 const PORT = 3000;
 app.listen(PORT, ()=> {
     console.log(`The Server is running on http://localhost:${PORT}`);
