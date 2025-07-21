fdd589d4b872e72ff3eb85218832ed39284704a4
surajit-tunga <surajittunga2005@gmail.com>
2025-07-21 20:34:02 +0530

    Added Styling

--- NOTES/11_DynamicUIUsingEJS/README.md
+++ NOTES/11_DynamicUIUsingEJS/README.md
@@ -1,1 +1,11 @@
 # Dynamic UI Using EJS
+
+- **Dynamic UI:** Changes in run time or as per user. (eg. diff feed of different in instagram).
+- **EJS:** Embaded JavaScript
+
+## Why we need Dynamic UI?
+- Personalized Content.
+- Dynamic Data Delivery: Provides real time info.
+- Security & Access control.
+- Localization: Adjust response as per language, culture..
+- API Versatility: Support Multi client types (web, mobile,iot).
--- airbnb/app.js
+++ airbnb/app.js
@@ -7,7 +7,7 @@ const express= require('express');
 // Import Routes
 const userRouter =require('./routes/userRouter');
 const hostRouter = require('./routes/hostRouter');
-const rootDir = require("./utils/pathUtils")
+const rootDir = require("./utils/pathUtils");
 
 
 const app = express();
@@ -20,10 +20,10 @@ app.use(express.urlencoded());
 app.use(userRouter);
 app.use("/host",hostRouter);
 
-app.use(express.static(path.join(rootDir, 'public')))
+app.use(express.static(path.join(rootDir, 'public')));
 
 app.use((req, res, next)=>{
-    res.sendFile(path.join(rootDir, 'views', '404.html'))
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
 })
 
 const PORT = 3000;
