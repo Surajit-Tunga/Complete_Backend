d366f34b658280d6e85791b376b4c28991aefa88
surajit-tunga <surajittunga2005@gmail.com>
2025-08-08 21:00:23 +0530

    Update save() for sql db

--- airbnb/models/home.js
+++ airbnb/models/home.js
@@ -1,17 +1,17 @@
 const db = require('../utils/databaseUtils');
 
 module.exports = class Home {
-    constructor(houseName, price, location, rating, desc, id){
+    constructor(houseName, price, location, rating, description, id){
         this.houseName = houseName;
         this.price =price;
         this.location = location;
         this.rating = rating;
-        this.desc =desc;
+        this.description =description;
         this.id =id;
     }
 
     save() {
-            
+        return db.execute(`INSERT INTO homes (houseName, location, price, rating, description) VALUES('${this.houseName}','${this.location}',${this.price},${this.rating},'${this.description}')`);          
    }
 
     static fetchAll() {
